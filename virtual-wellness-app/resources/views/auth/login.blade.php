ac5212546d0416233c2426183c7f9575ed3ebd0d
etoop <etoop@upei.ca>
2022-01-30 18:29:56 -0400

    log in frontend layout

--- virtual-wellness-app/resources/views/auth/login.blade.php
+++ virtual-wellness-app/resources/views/auth/login.blade.php
@@ -1,21 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container background" style="padding-top:5%">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+        <div class="col-md-5">
+            <div class="card" style="padding-top:60px; padding-bottom:60px">
                 <div class="card-header">{{ __('Login') }}</div>
-
+                <div class="card-subtitle" style="text-align:center">
+                                {{ __('Welcome! Login to access the Virtual Wellness App.') }}
+                                <br>
+                                @if (Route::has('password.request'))
+                                    {{ __('Did you')}}
+                                    <a class="btn btn-link" style="text-decoration:none; color:#629cdb; display:inline; padding:0" href="{{ route('password.request') }}">
+                                        {{__('forget your password?')}}
+                                    </a>
+                                @endif
+                </div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="row mb-3 justify-content-center">
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input placeholder= "Email Address" id="email" type="email" class="form-control @error('email') is-invalid @enderror" style="background-color:transparent; border-bottom: 1px solid rgba(0, 0, 0, 0.125);" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -25,11 +33,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="row mb-3 justify-content-center">
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input placeholder= "Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" style="background-color:transparent; border-bottom: 1px solid rgba(0, 0, 0, 0.125);" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -39,8 +46,8 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="row mb-3 justify-content-center">
+                            <div class="col-md-6 " style="align-items:center; justify-content:center; display:flex">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -51,18 +58,14 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="row mb-3 justify-content-center">
+                            <div class="col-md-6" style="align-items:center; justify-content:center; display:flex">
+                                <button type="submit" class="btn btn-primary" style="padding-left:100px; padding-right:100px;">
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
+                            
                         </div>
                     </form>
                 </div>
--- virtual-wellness-app/resources/views/layouts/app.blade.php
+++ virtual-wellness-app/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Virtual Wellness App') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -19,12 +19,12 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="py-4">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" style="color: #fcfcf8;" href="{{ url('/') }}">
+                    {{ config('app.name', 'Virtual Wellness App') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -33,7 +33,6 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
                     </ul>
 
                     <!-- Right Side Of Navbar -->
