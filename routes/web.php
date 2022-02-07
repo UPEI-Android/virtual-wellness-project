cdbb19dbaaec3c5f34723dd8e6ff2cb574ce3851
etoop <etoop@upei.ca>
2022-02-06 21:12:29 -0400

    added placeholder welcome to direct to login

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('/welcome');
+});
+
+Auth::routes();
+
+Route::get('/login', function () {
     return view('/auth/login');
 });
 
