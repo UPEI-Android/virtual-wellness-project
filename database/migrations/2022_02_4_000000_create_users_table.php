5cecaaa7bce845abe2f39f0afbf7071fc37c9047
JQuinn <jmquinn@upei.ca>
2022-02-07 16:50:11 -0400

    Adding fields to the Table for users

--- database/migrations/2022_02_4_000000_create_users_table.php
+++ database/migrations/2022_02_4_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->DATE('birthday');
+            $table->datetime('last_login_at')->nullable();
+            $table->string('last_login_ip')->nullable();
+            $table->foreignId('treatment_group')->nullable();
+            $table->foreignId('account_creator_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
