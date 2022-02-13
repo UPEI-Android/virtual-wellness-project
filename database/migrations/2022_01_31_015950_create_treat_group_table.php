8cd532ad25299bbebdc26e4cfe2e9dac3305c42a
JQuinn <jmquinn@upei.ca>
2022-02-13 11:29:18 -0400

    Adding table for treatment group

---
+++ database/migrations/2022_01_31_015950_create_treat_group_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDailyScheduleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('treatment_group', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('group_admin');
+            $table->foreignId('treatments');
+            $table->integer('members');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('daily_schedule');
+    }
+}
