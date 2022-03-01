8457d765ee74ed32ea1c526bf35697af669ff9b3
JQuinn <jmquinn@upei.ca>
2022-03-01 12:50:30 -0400

    renamed treatment table

--- database/migrations/2022_01_31_015950_create_treat_group_table.php
+++ database/migrations/2022_01_31_015950_create_treat_group_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDailyScheduleTable extends Migration
+class CreateTreatmentGroup extends Migration
 {
     /**
      * Run the migrations.
