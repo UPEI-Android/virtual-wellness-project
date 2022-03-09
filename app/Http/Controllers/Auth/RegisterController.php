2802a3a5ded43313dd96a737f08d3ba189cde4f3
Nikki Babaii <nbabaii@upei.ca>
2022-03-09 14:19:04 -0400

    jfkldjflds

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
     protected function create(Request $data)
     {
         return User::create([
-            'name' => $data['first_name'],
+            'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
