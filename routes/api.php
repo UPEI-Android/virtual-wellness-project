ff20441b29ea35a50ccce569f8f0a5bb85a426b1
Nikki Babaii <nbabaii@upei.ca>
2022-03-18 14:30:33 -0300

    Testing Sanctum Protection

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/userprofile/{id}', [\App\Http\Controllers\UserController::class,'getUser']) ->name('user.getUser');;
+Route::middleware('auth:sanctum')->get('/userprofile/{id}', [\App\Http\Controllers\UserController::class,'getUser']) ->name('user.getUser');;
 
-Route::apiResource('treatments', TreatmentController::class);
+Route::middleware('auth:sanctum')->apiResource('treatments', TreatmentController::class);
 
-Route::apiResource('treatments.rules', RulesController::class)
+Route::middleware('auth:sanctum')->apiResource('treatments.rules', RulesController::class)
     ->except('show')
     ->shallow();
