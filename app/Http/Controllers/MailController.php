33f2bf33d12cd1f07376936339792beddd86b668
Nameless7913 <jqu2@upei.ca>
2022-03-28 15:23:35 -0300

    Add date to the email message

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -5,16 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use Mail;
-use App\Models\User;
+use App\Models\Rule;
+use App\Services\RulesService;
+use RRule\RRule;
+use App\Models\Treatment;
 
 class MailController extends Controller
 {
-    public function basic_email(){
-        $users = User::all();
-        foreach ($users as $user){
-            $email = $user->email;
-            Mail::to($email)->queue(new \App\Mail\MyTestMail($user));
-            echo("Email is Sent to ".$email);
+    public function basic_email()
+    {
+        $rules = Rule::all();
+
+        foreach ($rules as $rule) {
+
+            switch ($rule->freq) {
+                case(1):
+                    $rule->freq = RRule::YEARLY;
+                    break;
+                case(2):
+                    $rule->freq = RRule::MONTHLY;
+                    break;
+                case(3):
+                    $rule->freq = RRule::WEEKLY;
+                    break;
+                default:
+                    $rule->freq = RRule::DAILY;
+            }
+
+            $rule_service = new RulesService($rule);
+            $treatment_id = $rule->treatment_id;
+            $treatments = DB::table('treatments')->where('id', '=', $treatment_id)->get();
+            foreach ($treatments as $treatment) {
+                $patient_id = $treatment->patient_id;
+                $users = DB::table('users')->where('id', '=', $patient_id)->get();
+                foreach ($users as $user) {
+                    $email = $user->email;
+                    Mail::to($email)->queue(new \App\Mail\MyTestMail($rule_service->nextOccurrence()[0], $user, $treatment));
+                    echo("Email is Sent to " . $email);
+                }
+            }
         }
+
     }
 }
--- app/Mail/MyTestMail.php
+++ app/Mail/MyTestMail.php
@@ -6,8 +6,6 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use App\Models\User;
-use App\Models\Treatment;
 use Carbon\Carbon;
 
 class MyTestMail extends Mailable
@@ -22,17 +20,16 @@ class MyTestMail extends Mailable
      *
      * @return void
      */
-    public function __construct(User $user)
+    public function __construct($date, $user,$treatment)
     {
         $this ->user  = $user;
         $name  = $user->first_name.' '.$user->last_name;
         $email = $user ->email;
-//        $date = Carbon::createFromDate(2022, 3, 1, -4);
-        $date = Carbon::tomorrow();
+        $title = $treatment->title;
         $diffInDays = $date->diff(Carbon::now())->days;
         $details = [
             'title' => 'Test email',
-            'body' => 'Hi '.$name.'. This is for testing email using smtp to '.$email.". You have ".$diffInDays." days left"
+            'body' => 'Hi '.$name.'. This is for testing email to '.$email.". You have ".$diffInDays." days left to complete ".$title
         ];
         $this->details = $details;
 
