120f5e253609a7705e40beadc71bbfae535735d5
JQuinn <jmquinn@upei.ca>
2022-04-11 11:32:01 -0300

    random hunch

--- app/Http/Controllers/TreatmentController.php
+++ app/Http/Controllers/TreatmentController.php
@@ -41,12 +41,14 @@ class TreatmentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TreatmentRequest $request)
     {
         //
+
+       // $this->write_to_console($request);
         return auth()->user()
             ->treatments()
-            ->create($request->all());
+            ->create($request->validated());
 
     }
 
@@ -78,13 +80,13 @@ class TreatmentController extends Controller
         //
     }
 
-//    function write_to_console($data) {
-//        $console = $data;
-//        if (is_array($console))
-//            $console = implode(',', $console);
-//
-//        echo "<script>console.log('Console: " . $console . "' );</script>";
-//    }
+    function write_to_console($data) {
+        $console = $data;
+        if (is_array($console))
+            $console = implode(',', $console);
+
+       echo "<script>console.log('Console: " . $console . "' );</script>";
+    }
 
     /**
      * Update the specified resource in storage.
@@ -93,17 +95,17 @@ class TreatmentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Treatment $treatment)
+    public function update(Request $request, int $id)
     {
-//        $treatment = Treatment::find($id);
-//
-//        if($treatment) {
-//
-//
-//            $treatment->update(($request['state']));
-//        }
+        $treatment = Treatment::find($id);
 
-        $treatment->update($request->all());
+      if($treatment) {
+
+
+            $treatment->update(($request['state']));
+        }
+
+       // $treatment->update($request->all());
         return response($treatment);
 
     }
