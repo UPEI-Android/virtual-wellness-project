6f6699abc780b3e58aca4ce9a5486e8041dbbf8f
etoop <etoop@upei.ca>
2022-04-13 14:49:41 -0300

    possible bandaid fix for reloading components on changes by trackin all local states

--- resources/js/components/Treatment/TreatmentList.jsx
+++ resources/js/components/Treatment/TreatmentList.jsx
@@ -7,55 +7,71 @@ import {getAllTreatments} from "../store/actions/AllTreatmentActions";
 
 function TreatmentList(props) {
   useEffect(()=>{
-    props.getAllTreatments()
-  },[])
+    props.getAllTreatments,
+    todosFiltered(filter)
+  },[shownList,allList, completeList, activeList, filter, props.todos])
 
-    const [filter, setFilter] = useState('all');
+  
+  const [filter, setFilter] = useState('all');
+  const [shownList, setShownList] = useState(props.todos)
+  const [allList, setAllList] =useState(props.todos)
+  const [completeList, setCompleteList] = useState(props.todos.filter(todo => todo.is_completed))
+  const [activeList, setActiveList] = useState(props.todos.filter(todo => !todo.is_completed))
+  
+  function remaining(){
+      return shownList.filter(todo => !todo.is_completed).length;
+    }
 
-    const [list, setList] = useState([]);
-
-    function remaining(){
-        return props.todos.filter(todo => !todo.is_completed).length;
+  function todosFiltered(filter){
+      if (filter === 'all') {
+        setShownList(allList)
+        return shownList
       }
-
-    function todosFiltered(filter){
-        if (filter === 'all') {
-          return props.todos;
-        }
-        else if (filter==='active'){
-          return props.todos.filter(todo => !todo.is_completed);
-        }
-        else if (filter==='completed'){
-          return props.todos.filter(todo => todo.is_completed);
-        }
+      else if (filter==='active'){
+        //setList($treatments.filter(todo => !todo.is_completed))
+        setShownList(activeList)
+        return shownList
       }
+      else if (filter==='completed'){
+        //setList($treatments.filter(todo => todo.is_completed))
+        setShownList(completeList)
+        return shownList
+      }
+    }
 
     function completeTodo(id){
-
-        // need to find specific treatment within the array of treatments
-        let $treatment = null;
-        let $i=0;
-        for($i;$i<props.singleTreatmentData.treatments.length;$i++){
-            if (props.singleTreatmentData.treatments[$i].id===id){
-                console.log(props.singleTreatmentData.treatments[$i].id)
-                $treatment = props.singleTreatmentData.treatments[$i]
-            }
+      // need to find specific treatment within the array of treatments
+      let $treatment = null;
+      props.singleTreatmentData.treatments.forEach((treatment)=>{
+        if(treatment.id===id){
+          $treatment = treatment
         }
-
+      })
 
       if ($treatment.id === id){
-        if($treatment.is_completed === 0)
-        {$treatment.is_completed =1}
-        else{$treatment.is_completed=0}
+        if($treatment.is_completed){
+          $treatment.is_completed=!$treatment.is_completed
+          setActiveList([...activeList, $treatment])
+          setCompleteList(completeList.filter(treatment => treatment.id != id))
+          
+        }
+        else{
+          $treatment.is_completed=!$treatment.is_completed
+          setCompleteList([...completeList, $treatment])
+          setActiveList(activeList.filter(treatment => treatment.id != id))
+        }
       }
-
         props.saveTreatmentData($treatment,id)
-        window.location.reload(false)
-      }
+    }
+
     function deleteTodo(id){
       props.deleteTreatment(id);
-      window.location.reload(false)
+      setAllList(allList.filter(treatment => treatment.id != id))
+      setActiveList(activeList.filter(treatment => treatment.id != id))
+      setCompleteList(completeList.filter(treatment => treatment.id != id))
+      setShownList(allList.filter(treatment => treatment.id != id))
     }
+
 
     return(
     <>
@@ -69,25 +85,26 @@ function TreatmentList(props) {
         role="list"
         className="list-unstyled"
     >
-        { todosFiltered(filter).map((todo, index) => (
-        <li key={todo.id} className="treatment-item-container">
-            <div className ="treatment-item">
-                <input type="checkbox" onChange={() => completeTodo(todo.id)} checked={todo.is_completed ? true : false}/>
-                <a href={'/treatment/' + todo.id} className="treatment-item treatment-list-item" >{ todo.title }</a>
+      {shownList.map((todo, index) => (
+      <li key={todo.id} className="treatment-item-container">
+          <div className ="treatment-item">
+              <input type="checkbox" onChange={() => completeTodo(todo.id)} checked={todo.is_completed ? true : false}/>
+              <a href={'/treatment/' + todo.id} className="treatment-item treatment-list-item" >{ todo.title }</a>
 
-            </div>
-            <div className="btn-group" style={{"display" : "block"}}>
-              {/*
-                <button type="button" className="btn" style={{"display" : "inline"}}>
-                Edit
-                </button>
-              */}
-                <button type="button" className="btn btn__danger"  onClick={()=> deleteTodo(todo.id)} style={{"display" : "inline"}}>
-                Delete
-                </button>
-            </div>
-        </li>
-        ))}
+          </div>
+          <div className="btn-group" style={{"display" : "block"}}>
+            {/*
+              <button type="button" className="btn" style={{"display" : "inline"}}>
+              Edit
+              </button>
+            */}
+              <button type="button" className="btn btn__danger"  onClick={()=> deleteTodo(todo.id)} style={{"display" : "inline"}}>
+              Delete
+              </button>
+          </div>
+      </li>
+      ))}
+        
     </ul>
 
     <div className="remaining-block">
